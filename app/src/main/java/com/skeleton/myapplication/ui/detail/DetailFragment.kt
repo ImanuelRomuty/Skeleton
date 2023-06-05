f015a07d6292b18044dbea6436bf3dba50a5cfe1
PoconGGaul <romuty16@gmail.com>
2023-06-05 19:30:17 +0700

    Search

--- app/src/main/java/com/skeleton/myapplication/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/skeleton/myapplication/ui/detail/DetailFragment.kt
@@ -2,23 +2,16 @@ package com.skeleton.myapplication.ui.detail
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.annotation.RequiresApi
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bumptech.glide.Glide
 import com.skeleton.myapplication.R
 import com.skeleton.myapplication.Utils
 import com.skeleton.myapplication.data.EventResult
 import com.skeleton.myapplication.databinding.FragmentDetailBinding
-import com.skeleton.myapplication.databinding.FragmentHomeBinding
 import com.skeleton.myapplication.ui.MainViewModel
-import com.skeleton.myapplication.ui.adapter.PhotosAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
@@ -30,7 +23,6 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
         super.onViewCreated(view, savedInstanceState)
         fetchData()
     }
-
     private fun fetchData() {
         viewModel.getDetail(argument.id).observe(viewLifecycleOwner) {
             when(it){
@@ -53,4 +45,5 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
             }
         }
     }
+
 }
